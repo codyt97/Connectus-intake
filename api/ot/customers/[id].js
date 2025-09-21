1697f805e88e1154bde18d7e0c2881495e89cd5d
codyt97 <87399610+codyt97@users.noreply.github.com>
2025-09-21 11:46:53 -0400

    Create [id].js

---
+++ api/ot/customers/[id].js
@@ -0,0 +1,11 @@
+import { getCustomerById } from "../../ot/_lib/ot.js";
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+  try {
+    const cust = await getCustomerById(Number(id));
+    res.status(200).json(cust);
+  } catch (e) {
+    res.status(502).json({ error: String(e.message || e) });
+  }
+}
